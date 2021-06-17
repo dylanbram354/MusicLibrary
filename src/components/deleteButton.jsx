b3a63f1bc47f08b1985df403bb339904786a2cc9
dylanbram354 <dylanbram354@gmail.com>
2021-06-17 14:10:51 -0500

    Completed table of all music

---
+++ src/components/deleteButton.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class DeleteButton extends Component {
+    constructor(props){
+        super(props);
+        song = props.song;
+    }
+}
--- src/components/musicTable.jsx
+++ src/components/musicTable.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import DeleteButton from './deleteButton'
 
 class MusicTable extends Component {
     constructor(props) {
@@ -13,7 +14,7 @@ class MusicTable extends Component {
 
     async getAllMusic(){
         try{
-            let response = await axios.get('http://localhost:8000/music/')
+            let response = await axios.get('http://localhost:8000/music/');
             this.setState({
                 allMusic: response.data
             })
@@ -25,11 +26,31 @@ class MusicTable extends Component {
 
     makeTable(){
         let header =[];
-        for (let key in this.state.allMusic[0])
-            header.push(<th>{key}</th>)
+        for (let key in this.state.allMusic[0]){
+            header.push(<th>{key}</th>);
+        }
+        let rows = [];
+        for (let i=0; i<this.state.allMusic.length; i++){
+            let currentSong = this.state.allMusic[i];
+            let row = [];
+            for (let key in currentSong){ //nested iteration boooooo 
+                row.push(
+                    <td>{currentSong[key]}</td>
+                )
+            }
+            row.push(
+                <td><DeleteButton id={currentSong[id]} /></td>
+            )
+            rows.push(
+                <tr>
+                    {row}
+                </tr>
+            )
+        }
         return(
             <table className="table">
                 <thead><tr>{header}</tr></thead>
+                <tbody>{rows}</tbody>
             </table>
         )
     }
@@ -39,6 +60,7 @@ class MusicTable extends Component {
             <React.Fragment>
                 {this.state.allMusic ? this.makeTable() : <h3 className="text-center">Generating table...</h3>}
             </React.Fragment>
+            //Do you pretty much always need conditional rendering when displaying data from API calls?
         )
     }
 }
